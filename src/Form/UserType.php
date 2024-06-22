48ce76260779a5b29ca3198b3127447c400bc303
Valentin Michel <valentin_michel@outlook.fr>
2024-06-22 03:17:15 +0200

    Exercices terminés

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -14,6 +14,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -39,13 +41,15 @@ final class UserType extends AbstractType
         $builder
             ->add('username', TextType::class, [
                 'label' => 'label.username',
-                'disabled' => true,
             ])
             ->add('fullName', TextType::class, [
                 'label' => 'label.fullname',
             ])
             ->add('email', EmailType::class, [
                 'label' => 'label.email',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'label.password',
             ])
         ;
     }
